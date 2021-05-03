57a3edb75564b9ea910406b4a921ec804b77eddf
Blaxion <80962519+Blaxion@users.noreply.github.com>
2021-05-03 10:54:06 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// Exo 1
+let personnage1 = {
+    nom:"Khibaziev",
+    prenom:"Farhad",
+    age:27,
+    taille:180
+}
+console.log(personnage1.age)
+
+//  Exo 2
+let personnage2 = {
+    nom:"Son",
+    prenom:"Goku",
+    age:50,
+    taille:190
+}
+
+let personnage3 ={}
+
+// Exo 3
+personnage3.nom = personnage1.nom
+personnage3.age = personnage2.age
+personnage3.taille = 250
+console.log(personnage3)
+
